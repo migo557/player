9fda844e3d10c414e01cb82ce2a65b5ce815ea6d
Furqan Uddin <frkudn@protonmail.com>
2024-12-15 22:27:05 +0500

    Changed app version

--- lib/base/strings/app_strings.dart
+++ lib/base/strings/app_strings.dart
@@ -2,7 +2,7 @@ final class AppStrings {
   static const String appName = "Open Player";
   static const String appNameSuffix = "Player";
   static const String appTagline = "An open-source audio & video player";
-  static const String appVersion = "v1.0";
+  static const String appVersion = "v1.1.0";
   static const String noAds = "No Ads";
   static const String noSubscription = "No Subscription";
   static const String noTracking = "No Trackings";

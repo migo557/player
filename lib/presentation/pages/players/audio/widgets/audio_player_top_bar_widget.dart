439a5976df52e1a92f6de0f4a61a717c8bc175ae
Furqan Uddin <frkudn@protonmail.com>
2024-12-25 03:40:58 +0500

    Added Speed Widget without logic and slide in animation on Slide Dialog

--- lib/presentation/pages/players/audio/widgets/audio_player_top_bar_widget.dart
+++ lib/presentation/pages/players/audio/widgets/audio_player_top_bar_widget.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
@@ -48,63 +49,93 @@ class AudioPlayerTopBarWidget extends StatelessWidget {
                 onPressed: () {
                   VxDialog.showCustom(
                     context,
-                    child: SizedBox(
-                      height: 150,
-                      width: mq.width * 0.25,
-                      child: [
-                        IconButton(
-                          onPressed: () {
+                    child: SlideInRight(
+                      child: SizedBox(
+                        height: 210,
+                        width: mq.width * 0.25,
+                        child: [
+                          //----- Add to Playlist
+                          StreamBuilder(
+                              stream: state.audioPlayer.currentIndexStream,
+                              builder: (context, snapshot) {
+                                final int? currentIndex = snapshot.data ??
+                                    state.audioPlayer.currentIndex;
+                                final AudioModel? audio = currentIndex != null
+                                    ? state.audios[currentIndex]
+                                    : null;
+                                return IconButton(
+                                  onPressed: () {
+                                    if (audio != null) {
+                                      context.pop();
+                                      showAddToPlaylistModalBottomSheet(
+                                          context, audio);
+                                    }
+                                  },
+                                  icon: Icon(
+                                    Icons.playlist_add_outlined,
+                                    color: Colors.white,
+                                    size: 35,
+                                  ),
+                                );
+                              }),
+                          "Add to Playlist".text.xs.white.make(),
+
+                          //------- Equalizer
+                          IconButton(
+                            onPressed: () {
                               context.pop();
-                            VxDialog.showCustom(
-                              context,
-                              child: SizedBox(
-                                height: 100,
-                                width: 200,
-                                child: "Currently not available"
-                                    .text
-                                    .white
-                                    .makeCentered()
-                                    .p8(),
-                              ).glassMorphic(),
-                            );
-                          },
-                          icon: Icon(
-                            Icons.equalizer,
-                            color: Colors.white,
-                            size: 35,
-                          ),
-                        ),
-                        "Equalizer".text.white.xs.make(),
-                        StreamBuilder(
-                            stream: state.audioPlayer.currentIndexStream,
-                            builder: (context, snapshot) {
-                              final int? currentIndex = snapshot.data ??
-                                  state.audioPlayer.currentIndex;
-                              final AudioModel? audio = currentIndex != null
-                                  ? state.audios[currentIndex]
-                                  : null;
-                              return IconButton(
-                                onPressed: () {
-                                  if (audio != null) {
-                                    context.pop();
-                                    showAddToPlaylistModalBottomSheet(
-                                        context, audio);
-                                  }
-                                },
-                                icon: Icon(
-                                  Icons.playlist_add_outlined,
-                                  color: Colors.white,
-                                  size: 35,
-                                ),
+                              VxDialog.showCustom(
+                                context,
+                                child: SizedBox(
+                                  height: 100,
+                                  width: 200,
+                                  child: "Currently not available"
+                                      .text
+                                      .white
+                                      .makeCentered()
+                                      .p8(),
+                                ).glassMorphic(),
                               );
-                            }),
-                        "Add to Playlist".text.xs.white.make(),
-                      ]
-                          .column(
-                            alignment: MainAxisAlignment.center,
-                          )
-                          .scrollVertical(),
-                    ).glassMorphic().pOnly(left: mq.width * 0.75),
+                            },
+                            icon: Icon(
+                              Icons.equalizer,
+                              color: Colors.white,
+                              size: 35,
+                            ),
+                          ),
+                          "Equalizer".text.white.xs.make(),
+
+                          //------- Speed Changer
+                          IconButton(
+                            onPressed: () {
+                              context.pop();
+                              VxDialog.showCustom(
+                                context,
+                                child: SizedBox(
+                                  height: 100,
+                                  width: 200,
+                                  child: "Currently not available"
+                                      .text
+                                      .white
+                                      .makeCentered()
+                                      .p8(),
+                                ).glassMorphic(),
+                              );
+                            },
+                            icon: Icon(
+                              Icons.speed,
+                              color: Colors.white,
+                              size: 35,
+                            ),
+                          ),
+                          "Speed".text.white.xs.make(),
+                        ]
+                            .column(
+                              alignment: MainAxisAlignment.center,
+                            )
+                            .scrollVertical(),
+                      ).glassMorphic().pOnly(left: mq.width * 0.75),
+                    ),
                   );
                 },
                 color: Colors.white,
